0d0696f81bfcf99105b648d95168f538df0910f8
bluepome98 <wpdn1998@gmail.com>
2025-05-20 21:22:48 +0900

    .prettierrc 내용 추가, Header 컴포넌트 기본 구조 생성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,25 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
 const Header = () => {
   return (
-    <header className="w-full h-[80px] px-[40px] py-[24px] flex items-center bg-backgroundWhite">
+    <header className="flex h-[80px] w-full items-center bg-backgroundWhite px-[40px] py-[24px]">
       <div className="flex-1">
         <Link href="/">
           <Image src="/header.svg" alt="logo" width={180} height={30} />
         </Link>
       </div>
 
-      <nav className="flex flex-1 justify-between ">
-        <Link
-          href="/note"
-          className="text-labelNeutral text-[16px] font-medium"
-        >
+      <nav className="flex flex-1 justify-between">
+        <Link href="/note" className="text-[16px] font-medium text-labelNeutral">
           걱정 작성
         </Link>
-        <Link
-          href="/notebox"
-          className="text-labelNeutral text-[16px] font-medium"
-        >
+        <Link href="/notebox" className="text-[16px] font-medium text-labelNeutral">
           걱정 보관함
         </Link>
       </nav>
-      
-      <div className="flex flex-1">로그인</div>
+
+      <div className="flex flex-1 items-center justify-center text-[16px] font-medium text-labelNeutral">로그인</div>
     </header>
   );
 };

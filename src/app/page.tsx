d975facf11dcd58843f2547fc88ce700900c7adc
bluepome98 <wpdn1998@gmail.com>
2025-05-24 00:30:49 +0900

    소개 페이지 다시 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 
 const App = () => {
-  return (
-    <div className="bg-backgroundWhite px-[12px] py-[24px]">
-      <section className="relative h-[540px] w-full rounded-xl">
-        <Image src="/hero-section-desktop.svg" fill={true} alt="intro" className="object-cover" />
-      </section>
-      <footer className="h-[48px] w-full bg-gray-500"></footer>
-    </div>
-  );
+    return (
+        <section className="bg-backgroundWhite px-[12px] py-[24px]">
+            <section className="relative min-h-screen w-full overflow-hidden">
+                <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
+                    <Image
+                        src="/hero-section-desktop.svg"
+                        fill
+                        alt="hero-section"
+                        className="object-cover"
+                    />
+                </div>
+            </section>
+            <footer className="h-[48px] w-full bg-gray-500"></footer>
+        </section>
+    );
 };
 
 export default App;

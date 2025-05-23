c78b0ec769ecd215ea20751120ee346b0b43edd5
bluepome98 <wpdn1998@gmail.com>
2025-05-24 00:41:04 +0900

    소개 페이지 이미지, 레이아웃 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,53 @@ import Image from 'next/image';
 const App = () => {
     return (
         <section className="bg-backgroundWhite px-[12px] py-[24px]">
-            <section className="relative min-h-screen w-full overflow-hidden">
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
                 <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
                     <Image
                         src="/hero-section-desktop.svg"
                         fill
                         alt="hero-section"
-                        className="object-cover"
+                        className="object-fill"
+                    />
+                </div>
+            </section>
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
+                <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
+                    <Image
+                        src="/noteIntro.svg"
+                        fill
+                        alt="hero-section"
+                        className="object-fill"
+                    />
+                </div>
+            </section>
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
+                <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
+                    <Image
+                        src="/staticIntro.svg"
+                        fill
+                        alt="hero-section"
+                        className="object-fill"
+                    />
+                </div>
+            </section>
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
+                <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
+                    <Image
+                        src="/letterIntro.svg"
+                        fill
+                        alt="hero-section"
+                        className="object-fill"
+                    />
+                </div>
+            </section>
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
+                <div className="relative h-[540px] w-full overflow-hidden rounded-xl">
+                    <Image
+                        src="/communityIntro.svg"
+                        fill
+                        alt="hero-section"
+                        className="object-fill"
                     />
                 </div>
             </section>

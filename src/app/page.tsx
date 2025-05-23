4a9ebd8960cbc2f43603fad28d31431a497e1c4b
bluepome98 <wpdn1998@gmail.com>
2025-05-23 16:34:50 +0900

    메인 페이지 히어로 섹션 이미지 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Link from 'next/link';
-
+import Image from 'next/image';
 const App = () => {
-  return <div className="bg-backgroundWhite"></div>;
+  return (
+    <div className="bg-backgroundWhite px-[12px] py-[24px]">
+      <section className="relative h-[540px] w-full rounded-xl">
+        <Image src="/hero-section-desktop.svg" fill={true} alt="intro" className="object-cover" />
+      </section>
+      <footer className="h-[48px] w-full bg-gray-500"></footer>
+    </div>
+  );
 };
 
 export default App;

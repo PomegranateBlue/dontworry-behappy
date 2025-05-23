28580d2b91bc7d71a65ed939e780d2dcd46f1c87
bluepome98 <wpdn1998@gmail.com>
2025-05-23 14:16:56 +0900

    Zustand 패키지 설치

---
+++ src/app/notebox/page.tsx
@@ -0,0 +1,9 @@
+const NoteBox = () => {
+  return (
+    <div>
+      <p>This is NoteBox page</p>
+    </div>
+  );
+};
+
+export default NoteBox;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 const Header = () => {
   return (
-    <header className="flex h-[80px] w-full items-center bg-backgroundWhite px-[40px] py-[24px]">
+    <header className="flex h-[64px] w-full items-center bg-backgroundWhite px-[40px] py-[24px]">
       <div className="flex-1">
         <Link href="/">
           <Image src="/header.svg" alt="logo" width={180} height={30} />
         </Link>
       </div>
 
-      <nav className="flex flex-1 justify-between bg-red-500">
+      <nav className="flex flex-1 justify-around bg-red-500">
         <Link href="/note" className="text-[16px] font-medium text-labelNeutral">
           걱정 작성
         </Link>

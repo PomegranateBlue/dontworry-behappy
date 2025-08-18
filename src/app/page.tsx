74319eb503ab86cb32b730536367d2ca570ecad1
bluepome98 <wpdn1998@gmail.com>
2025-08-18 20:51:55 +0900

    feat : ./alyout.tsx로의 Header 컴포넌트 호출 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@
 //컴포넌트로 반복 구조 수정하기
 
 import Image from 'next/image';
-import Header from '@/components/common/Header';
+
 const App = () => {
     return (
-        <>
-            <Header />
-            <main className="bg-backgroundWhite px-3 py-6 pt-16">
-                <section className="relative mx-auto min-h-screen w-full overflow-hidden">
+        <main className="bg-backgroundWhite px-3 py-6 pt-16">
+            <section className="relative mx-auto min-h-screen w-full overflow-hidden">
                 <div className="relative aspect-video w-full overflow-hidden rounded-xl">
                     <Image
                         src="/hero-section-desktop.svg"
@@ -60,8 +58,7 @@ const App = () => {
             </section>
             <div></div>
             <footer className="h-12 w-full bg-gray-500"></footer>
-            </main>
-        </>
+        </main>
     );
 };
 

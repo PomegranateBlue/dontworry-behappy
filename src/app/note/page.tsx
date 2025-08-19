473128d81ce4110099ce41982961f2e96c0350af
bluepome98 <wpdn1998@gmail.com>
2025-08-19 16:44:19 +0900

    install : luicid-react 설치

---
+++ src/app/note/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+const NotePage = () => {
+    return (
+        <main>
+            <span>이야기 나누기</span>
+            <p>말을 하거나 텍스트 입력으로 자유롭게 고민을 털어보세요</p>
+            <div>
+                <button>Record</button>
+            </div>
+            <span>버튼을 누르시면 녹음을 할 수 있습니다</span>
+        </main>
+    );
+};
+
+export default NotePage;
